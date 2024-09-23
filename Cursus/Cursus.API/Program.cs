f643fc70470315e50be07620ace6ea387786dd03
NET_13_LeXuanPhuongNam <namlxpse173365@fpt.edu.vn>
2024-09-23 11:25:17 +0700

    IdentityUser Application

--- Cursus/Cursus.API/Program.cs
+++ Cursus/Cursus.API/Program.cs
@@ -1,3 +1,8 @@
+using Cursus.Data.Entities;
+using Cursus.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace Cursus.API
 {
     public class Program
@@ -7,10 +12,35 @@ namespace Cursus.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<CursusDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<CursusDbContext>()
+            .AddDefaultTokenProviders();
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
+            {
+                options.Password.RequiredLength = 12;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireDigit = true; 
+                options.Password.RequireUppercase = true;
+                options.User.RequireUniqueEmail = true; // Yêu c?u email ph?i duy nh?t
+                options.SignIn.RequireConfirmedEmail = false; // Yêu c?u xác nh?n email khi ??ng nh?p
+            })
+            .AddEntityFrameworkStores<CursusDbContext>()
+            .AddDefaultTokenProviders();
+
+
 
             builder.Services.AddControllers();
 
             var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<CursusDbContext>();
+                dbContext.Database.Migrate(); 
+            }
 
             // Configure the HTTP request pipeline.
 
@@ -18,6 +48,7 @@ namespace Cursus.API
 
             app.UseAuthorization();
 
+            app.UseAuthentication();
 
             app.MapControllers();
 
---
+++ Cursus/Cursus.Data/Models/CursusDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Cursus.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cursus.Data.Models
+{
+    public class CursusDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
+    {
+        public CursusDbContext(DbContextOptions<CursusDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var studentId = "3f98d917-eca4-4813-98ab-b17810eda3dc";
+            var adminId = "f58c280f-f3b4-4b1f-903c-485b33cf3490";
+            var instructorId = "f5a5e287-ac21-4ba5-be8d-353df9689a8e";
+
+            var roles = new List<IdentityRole<Guid>>
+            {
+                new IdentityRole<Guid>
+                {
+                    Id = Guid.Parse(studentId),
+                    ConcurrencyStamp = studentId,
+                    Name = "Student",
+                    NormalizedName = "STUDENT"
+                },
+                new IdentityRole<Guid>
+                {
+                    Id = Guid.Parse(adminId),
+                    ConcurrencyStamp = adminId,
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole<Guid>
+                {
+                    Id = Guid.Parse(instructorId),
+                    ConcurrencyStamp = instructorId,
+                    Name = "Instructor",
+                    NormalizedName = "INSTRUCTOR"
+                }
+            };
+
+            builder.Entity<IdentityRole<Guid>>().HasData(roles);
+        }
+    }
+}
