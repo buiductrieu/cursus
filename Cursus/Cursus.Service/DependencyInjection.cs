010827d884f10bcb0daa53797fc5f4a960a7b9b7
NHQHwng <nguyenhung.5703@gmail.com>
2024-09-23 21:19:46 +0700

    add dependency injection

---
+++ Cursus/Cursus.Service/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Cursus.Service.Services;
+using Cursus.ServiceContract.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cursus.Service
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddService(this IServiceCollection services)
+        {
+            // DI Service
+            services.AddTransient<ICategoryService, CategoryService>();
+
+            return services;
+        }
+    }
+}
