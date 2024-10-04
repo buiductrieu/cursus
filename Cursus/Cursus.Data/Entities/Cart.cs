8975ae6a4dc7cd8d9cf0dc1e58f224699cb613b3
Phạm Anh Kiệt <kietpase183731@fpt.edu.vn>
2024-10-04 16:43:15 +0700

    Cart Database basic create

---
+++ Cursus/Cursus.Data/Entities/Cart.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cursus.Data.Entities
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public dateTime DateCreated { get; set; }
+        
+        public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+    }
+}
