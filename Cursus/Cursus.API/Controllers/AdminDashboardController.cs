4458329fcf9fea3d34893f60e76e916115a871d3
voleducanh93 <voleducanh93@gmail.com>
2024-10-31 12:14:09 +0700

    Setup AdminDashboard TG-223 #CLOSED

---
+++ Cursus/Cursus.API/Controllers/AdminDashboardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cursus.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminDashboardController : ControllerBase
+    {
+    }
+}
