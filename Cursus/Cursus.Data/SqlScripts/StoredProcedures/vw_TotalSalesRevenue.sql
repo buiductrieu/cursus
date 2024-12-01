dad801ebf0c0792a2578b5bf2c7fedc9f0b6e9a5
Duc1810 <146954012+Duc1810@users.noreply.github.com>
2024-12-01 13:45:25 +0700

    -Fit_DashBoard

--- Cursus/Cursus.Data/SqlScripts/StoredProcedures/vw_TotalSalesRevenue.sql
+++ Cursus/Cursus.Data/SqlScripts/StoredProcedures/vw_TotalSalesRevenue.sql
@@ -1,18 +1,17 @@
-IF OBJECT_ID('dbo.vw_TotalSalesRevenue', 'V') IS NOT NULL
+﻿IF OBJECT_ID('dbo.vw_TotalSalesRevenue', 'V') IS NOT NULL
     DROP VIEW dbo.vw_TotalSalesRevenue;
 GO
 CREATE VIEW dbo.vw_TotalSalesRevenue
 WITH SCHEMABINDING AS
 SELECT 
     COUNT_BIG(*) AS RecordCount,
-    COUNT_BIG(ci.CartItemsId) AS TotalSales,
-    SUM(CASE WHEN o.Status = 1 THEN o.PaidAmount ELSE 0 END) AS TotalRevenue,
+    COUNT_BIG(o.OrderId) AS TotalSales, 
+    SUM(CASE WHEN o.Status = 1 THEN o.PaidAmount ELSE 0 END) AS TotalRevenue, 
     o.Status,
-    o.DateCreated
-FROM dbo.CartItems ci
-INNER JOIN dbo.[Order] o ON ci.CartId = o.CartId
+    CONVERT(DATE, o.DateCreated) AS DateCreated 
+FROM dbo.[Order] o
 WHERE o.Status = 1
-GROUP BY o.Status, o.DateCreated;
+GROUP BY o.Status, CONVERT(DATE, o.DateCreated);
 GO
 CREATE UNIQUE CLUSTERED INDEX IDX_vw_TotalSalesRevenue 
 ON dbo.vw_TotalSalesRevenue (Status, DateCreated);
