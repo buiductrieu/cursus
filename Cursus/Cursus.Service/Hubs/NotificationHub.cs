01b412b821c72c37bfae61f8d0bc0a2459ece51a
Sơn Tom <sonnqse170489@fpt.edu.vn>
2024-12-02 03:08:14 +0700

    done Notification system

--- Cursus/Cursus.Service/Hubs/NotificationHub.cs
+++ Cursus/Cursus.Service/Hubs/NotificationHub.cs
@@ -14,13 +14,7 @@ namespace Cursus.Service.Hubs
 
 		public async Task SendNotificationToUser(string userId, string message)
 		{
-			await Clients.User(userId).SendAsync("ReceiveNotification", "message");
-		}
-
-		public async Task GetUserNotifications(string userId)
-		{
-			var notifications = await _notificationService.FetchNotificationsAsync(userId);
-			await Clients.Caller.SendAsync("LoadNotifications", notifications);
+			await Clients.User(userId).SendAsync("ReceiveNotification", userId, message);
 		}
 	}
 }
